87cb4e4020c52a65bdf7155bfa416f391d89f274
Konstantin.ShcherbininKonstantin.Shcherbinin <kshcherbinin@mail.ru>
2020-03-24 18:11:05 +0500

    Deleted cycle FOR and common comment

--- module_0/main.py
+++ module_0/main.py
@@ -1,27 +1,23 @@
-def game_core(number):
+def game_core(predict):
     num_1 = 1
     num_2 = 100
-    count = 0
+    count = 1
     text_1 = 'Загаданное число ='
     text_2 = 'Использованое число попыток ='
     import random
     import numpy as np
-    predict=np.random.randint(num_1, num_2)
-    while number != predict and count != num_2: #Запускаем цикл для подсчета количества попыток.
-        count += 1
-        for count in range(num_1, num_2): #Запускаем подцикл для проверки на больше/меньше среднего и изменения границ диапазона.
-            number = (num_1 + num_2)//2
-            if predict > number:
-                num_1 = (num_1 + num_2)//2
-                count += 1        
-            elif predict < number:
-                num_2 = (num_1 + num_2)//2
-                count += 1
-            else: 
-             break
-        print(text_2, count)
-    else: 
-        print(text_1, predict)
+    number=np.random.randint(num_1, num_2)
+    while number != predict and count < num_2:
+        predict = (num_1 + num_2)//2
+        count +=1
+        if predict < number:
+            num_1 = (num_1 + num_2)//2
+        elif predict > number:
+            num_2 = (num_1 + num_2)//2
+        else: 
+            break
+    print(text_1, predict)
+    print(text_2, count)
     return(count)
 
 def score_game(game_core):
@@ -31,8 +27,8 @@ def score_game(game_core):
     count_ls = []
     np.random.seed(1)
     random_array = np.random.randint(num_1, num_2 + 1, size=(1000))
-    for number in random_array:
-        count_ls.append(game_core(number))
+    for predict in random_array:
+        count_ls.append(game_core(predict))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
